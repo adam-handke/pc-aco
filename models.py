c525de2db4157ad70c2a058c6bf3afd6491839d7
adam-handke <adam.handke@outlook.com>
2023-08-06 03:29:28 +0200

    add: MSCVF model.

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import warnings
 import numpy as np
-from pulp import LpProblem, LpVariable, LpMaximize, PULP_CBC_CMD
+from pulp import LpProblem, LpVariable, LpMaximize, LpMinimize, PULP_CBC_CMD
 
 
 class Model:
@@ -159,6 +159,122 @@ class MinimalSlopeChangeValueFunction(Model):
     def __str__(self):
         return 'MSCVF'
 
+    def update(self, compared_pair):
+        super().update(compared_pair)
+        while len(self.buffer) > 0:
+            # first LP - determine the epsilon using the PRVF/MDVF model
+            if self.const_eps is None:
+                eps_model = MostDiscriminatingValueFunction(self.buffer_max_size, self.objectives, False,
+                                                            self.buffer, self.best_obj, self.worst_obj)
+                epsilon, _, _, _, _ = eps_model.solve_linear_programming_problem()
+                eps = epsilon.varValue
+            else:
+                eps = self.const_eps
+            if eps <= 0:
+                if self.verbose:
+                    print(f'Eps-LP solved but the preferences are incompatible (epsilon={eps}) '
+                          f'- discarding the oldest pair ({len(self.buffer) - 1} will remain)', flush=True)
+                self.buffer.pop(0)
+                if len(self.buffer) == 0:
+                    warnings.warn('Unable to perform model update - no pairs left in the buffer!')
+            else:
+                if self.verbose and self.const_eps is None:
+                    print(f'Eps-LP solved and the preferences are compatible (epsilon={eps}); '
+                          f'now solving the main LP', flush=True)
+                # second LP - solve the actual MSCVF problem using the known epsilon
+                # just like in the paper (J. Branke et al.), epsilon is divided by 1000
+                if self.const_eps is None:
+                    eps /= 1000.0
+
+                lp = LpProblem(name='MSVF', sense=LpMinimize)
+                u_better = [[LpVariable(f'ub{obj}_{pair}') for obj in range(self.objectives)]
+                            for pair in range(len(self.buffer))]
+                u_worse = [[LpVariable(f'uw{obj}_{pair}') for obj in range(self.objectives)]
+                           for pair in range(len(self.buffer))]
+                u_best = [LpVariable(f'ubest{obj}') for obj in range(self.objectives)]
+                u_worst = [LpVariable(f'uworst{obj}') for obj in range(self.objectives)]
+                rho = LpVariable('rho')
+
+                # minimized rho
+                lp += rho
+
+                # rho constraints - based on equation (5) from the paper
+                for obj in range(self.objectives):
+                    obj_util_list = [[self.best_obj[obj], u_best[obj]], [self.worst_obj[obj], u_worst[obj]]]
+                    for pair in range(len(self.buffer)):
+                        obj_util_list.append([self.buffer[pair][0][obj], u_better[pair][obj]])
+                        obj_util_list.append([self.buffer[pair][1][obj], u_worse[pair][obj]])
+                    obj_util_list.sort(key=lambda x: x[0])
+                    for k in range(2, len(obj_util_list)):
+                        if ((obj_util_list[k][0] - obj_util_list[k-1][0]) > 0
+                                and (obj_util_list[k-1][0] - obj_util_list[k-2][0]) > 0):
+                            lp += (((obj_util_list[k][1] - obj_util_list[k-1][1])
+                                    / (obj_util_list[k][0] - obj_util_list[k-1][0]))
+                                   - ((obj_util_list[k-1][1] - obj_util_list[k-2][1])
+                                      / (obj_util_list[k-1][0] - obj_util_list[k-2][0]))) <= rho
+
+                        if ((obj_util_list[k-1][0] - obj_util_list[k-2][0]) > 0
+                                and (obj_util_list[k][0] - obj_util_list[k-1][0]) > 0):
+                            lp += (((obj_util_list[k-1][1] - obj_util_list[k-2][1])
+                                    / (obj_util_list[k-1][0] - obj_util_list[k-2][0]))
+                                   - ((obj_util_list[k][1] - obj_util_list[k-1][1])
+                                      / (obj_util_list[k][0] - obj_util_list[k-1][0]))) <= rho
+
+                # monotonicity constraints
+                for obj in range(self.objectives):
+                    for pair1 in range(len(self.buffer)):
+                        for pair2 in range(len(self.buffer)):
+                            for p1 in range(2):  # which solution in pair1
+                                for p2 in range(2):  # which solution in pair2
+                                    if pair1 != pair2 or p1 != p2:
+                                        if self.buffer[pair1][p1][obj] < self.buffer[pair2][p2][obj]:
+                                            lp += ((u_better[pair1][obj] if p1 == 0 else u_worse[pair1][obj])
+                                                   >= (u_better[pair2][obj] if p2 == 0 else u_worse[pair2][obj]))
+                                        elif self.buffer[pair1][p1][obj] == self.buffer[pair2][p2][obj]:
+                                            # making sure that the same objective value always has the same utility
+                                            lp += ((u_better[pair1][obj] if p1 == 0 else u_worse[pair1][obj])
+                                                   == (u_better[pair2][obj] if p2 == 0 else u_worse[pair2][obj]))
+                for obj in range(self.objectives):
+                    for pair in range(len(self.buffer)):
+                        if self.buffer[pair][0][obj] < self.worst_obj[obj]:
+                            lp += u_better[pair][obj] >= u_worst[obj]
+                        elif self.buffer[pair][0][obj] == self.worst_obj[obj]:
+                            lp += u_better[pair][obj] == u_worst[obj]
+
+                        if self.buffer[pair][1][obj] < self.worst_obj[obj]:
+                            lp += u_worse[pair][obj] >= u_worst[obj]
+                        elif self.buffer[pair][1][obj] == self.worst_obj[obj]:
+                            lp += u_worse[pair][obj] == u_worst[obj]
+
+                        if self.best_obj[obj] < self.buffer[pair][0][obj]:
+                            lp += u_best[obj] >= u_better[pair][obj]
+                        elif self.best_obj[obj] == self.buffer[pair][0][obj]:
+                            lp += u_best[obj] == u_better[pair][obj]
+
+                        if self.best_obj[obj] < self.buffer[pair][1][obj]:
+                            lp += u_best[obj] >= u_worse[pair][obj]
+                        elif self.best_obj[obj] == self.buffer[pair][1][obj]:
+                            lp += u_best[obj] == u_worse[pair][obj]
+                    lp += u_best[obj] >= u_worst[obj]
+
+                # pairwise preference constraints
+                for pair in range(len(self.buffer)):
+                    lp += (sum([u_better[pair][obj] for obj in range(self.objectives)])
+                           - sum([u_worse[pair][obj] for obj in range(self.objectives)])) >= eps
+
+                # normalization constraint (lower bound)
+                lp += sum([u_best[obj] for obj in range(self.objectives)]) == 1.0
+
+                # normalization constraint (upper bound)
+                for obj in range(self.objectives):
+                    lp += u_worst[obj] == 0.0
+
+                lp.solve(PULP_CBC_CMD(msg=False))
+                if self.verbose:
+                    print(f'Main LP solved (rho={rho.varValue})', flush=True)
+                self.translate_interpolation_points(u_best, u_worst, u_better, u_worse)
+                break
+
 
 class MaximalSumOfScoresValueFunction(Model):
     def __str__(self):
@@ -255,6 +371,8 @@ class MaximalSumOfScoresValueFunction(Model):
                     lp += u_worst[obj] == 0.0
 
                 lp.solve(PULP_CBC_CMD(msg=False))
+                if self.verbose:
+                    print(f'Main LP solved', flush=True)
                 self.translate_interpolation_points(u_best, u_worst, u_better, u_worse)
                 break
 
